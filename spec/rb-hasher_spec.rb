fc1faa1780bf53575fb2bb074f14ff26a1e95f5b
Victor Guzman <victor.guzman@runawaybit.com>
2014-12-09 14:08:50 -0600

    Add specs when value has spaces in the middle

--- spec/rb-hasher_spec.rb
+++ spec/rb-hasher_spec.rb
@@ -8,38 +8,48 @@ describe RB::Hasher do
   describe '#generate' do
     let(:input) { ['ee','rrr','q','','ttttt'] }
     let(:output) { 4177694326 }
+    
+    shared_examples 'returns the right hash value' do
+      specify { expect(hasher.generate(input)).to eq output }
+    end
 
     context 'when all values are lowercase' do
-      it 'returns the right hash value' do
-        expect(hasher.generate(input)).to eq output
-      end
+      it_behaves_like 'returns the right hash value'
     end
 
     context 'when all values are uppercase' do
-      
       let(:input) { ['EE','RRR','Q','','TTTTT'] }
-
-      it 'returns the right hash value' do
-        expect(hasher.generate(input)).to eq output
-      end
+      it_behaves_like 'returns the right hash value'
     end
 
     context 'when values are uppercase and lowercase' do
-      
       let(:input) { ['ee','RRR','Q','','ttttt'] }
-
-      it 'returns the right hash value' do
-        expect(hasher.generate(input)).to eq output
-      end
+      it_behaves_like 'returns the right hash value'
     end
 
     context 'when values have leading and/or trailing spaces' do
-      
       let(:input) { [' ee','rrr',' q ','','ttttt  '] }
-
-      it 'returns the right hash value' do
-        expect(hasher.generate(input)).to eq output
-      end
+      it_behaves_like 'returns the right hash value'
     end
+
+    context 'when a value is a single space' do
+      let(:input) { ['ee','rrr','q',' ','ttttt'] }
+      it_behaves_like 'returns the right hash value'
+    end
+
+    context 'when a value has a space in the middle' do
+      let(:output) { 4106923319 }
+
+      context 'when values have single space in the middle' do
+        let(:input) { ['ee','rrr','q','','tt ttt'] }
+        it_behaves_like 'returns the right hash value'
+      end
+
+      context 'when a value has multiple spaces in the middle' do
+        let(:input) { ['ee','rrr','q','','tt   ttt'] }
+        it_behaves_like 'returns the right hash value'
+      end
+    end # when has space in middle
+
   end
 end

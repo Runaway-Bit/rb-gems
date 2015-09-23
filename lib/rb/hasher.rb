29ec7a09b011414df4dbb98ecfdb24f18460f92f
Roberto Fernandez <roberto@runawaybit.com>
2015-09-22 20:30:50 -0600

    Revert "Change Murmurhash from V32 to V128"

--- lib/rb/hasher.rb
+++ lib/rb/hasher.rb
@@ -8,18 +8,18 @@ module RB
       if values.count == 1
         first_value = values.first
         if first_value.is_a?(Hash)
-          MurmurHash3::V128.str_hash(
+          MurmurHash3::V32.str_hash(
             first_value.sort.map(&:last).join(' ').squeeze(' ').strip.upcase
-          ).first(2).join('')
+          )
         else
-          MurmurHash3::V128.str_hash(
+          MurmurHash3::V32.str_hash(
             first_value.join(' ').squeeze(' ').strip.upcase
-          ).first(2).join('')
+          )
         end
       else
-        MurmurHash3::V128.str_hash(
+        MurmurHash3::V32.str_hash(
             values.join(' ').squeeze(' ').strip.upcase
-          ).first(2).join('')
+          )
       end
     end
 
--- lib/rb/hasher/version.rb
+++ lib/rb/hasher/version.rb
@@ -1,5 +1,5 @@
 module Rb
   module Hasher
-    VERSION = "1.0.0"
+    VERSION = "0.0.1"
   end
 end
